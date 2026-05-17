7cb21291a6e6c90a0049f460d87397d195941a42
Mico46 <109225511+Mico46@users.noreply.github.com>
2026-05-17 20:15:48 +0300

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path');
 
 // storage config
-const storage = multer.diskStorage({
+/*const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads/');
   },
@@ -13,6 +13,18 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage });*/
+
+
+
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage,
+
+  limits: {
+    fileSize: 10 * 1024 * 1024,
+  },
+});
 
 module.exports = upload;
