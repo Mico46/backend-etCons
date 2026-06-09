fd46882b610acab90dc03a9aa9da1b1d07b8d1f9
Mico46 <tex.mic22@gmail.com>
2026-06-09 21:11:00 +0300

    site model

--- models/siteModel.js
+++ models/siteModel.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
 const siteSchema = new mongoose.Schema({
-  name: String,
-  location: String,
+ 
   userId: String,
+   name: String,
+  clientName: String,
+  locationName: String,
+  budget: Number,
+  startDate: Date,
+  targetEndDate: Date
 }, { timestamps: true });
 
 module.exports = mongoose.model('Site', siteSchema);
